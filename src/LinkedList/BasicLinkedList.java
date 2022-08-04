55e08a4528066c999098dea897fe816a69d8b227
Hoai Thu <dangnguyenhoaithu08012000@gmail.com>
2022-08-04 17:31:57 +0700

    add method in linkedlist: insert nth position, reverse linked list, delete n th linked list, print linked list recursion

--- src/LinkedList/BasicLinkedList.java
+++ src/LinkedList/BasicLinkedList.java
@@ -5,6 +5,8 @@ import org.w3c.dom.Node;
 public class BasicLinkedList {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+
+        System.out.println("Print linked list after inserting");
         linkedList = LinkedList.insert(linkedList, 1);
         linkedList = LinkedList.insert(linkedList, 2);
         linkedList = LinkedList.insert(linkedList, 3);
@@ -12,5 +14,22 @@ public class BasicLinkedList {
         linkedList = LinkedList.insert(linkedList, 4);
         linkedList = LinkedList.add(linkedList, 7);
         LinkedList.printLinkedList(linkedList);
+
+        System.out.println("Print linked list after inserting nth position");
+        linkedList = LinkedList.insertNthPosition(linkedList, 8, 1);
+        linkedList = LinkedList.insertNthPosition(linkedList, 10, 10);
+        LinkedList.printLinkedList(linkedList);
+
+        System.out.println("Print linked list after deleting nth position");
+        linkedList = LinkedList.deleteNthPosition(linkedList, 3);
+        linkedList = LinkedList.deleteNthPosition(linkedList, 0);
+        LinkedList.printLinkedList(linkedList);
+
+        System.out.println("Print linked list after reversing");
+        linkedList = LinkedList.reverse(linkedList);
+        LinkedList.printLinkedList(linkedList);
+
+        System.out.println("Print recursion");
+        LinkedList.printLinkedListRecursion(linkedList.head);
     }
 }
--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -26,13 +26,61 @@ public class LinkedList {
         }
         return list;
     }
+    public static LinkedList insertNthPosition (LinkedList list, int data, int position){
+        if (position == 0){
+            return add(list, data);
+        }
+        Node newNode = new Node(data);
+        Node temp = list.head;
+        for (int i = 0; i <= position - 2; i ++){
+            temp = temp.next;
+            if (temp == null){
+                System.out.println("Invalid position");
+                return list;
+            }
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        return list;
+    }
+    public static LinkedList deleteNthPosition (LinkedList list, int position)
+    {
+        if (position == 0){
+            list.head = list.head.next;
+        }
+        else {
+            Node temp = list.head;
+            for (int i = 0; i <= position - 2; i ++){
+                temp = temp.next;
+                if (temp == null){
+                    System.out.println();
+                    System.out.println("Invalid position");
+                    return list;
+                }
+            }
+            temp.next = temp.next.next;
+        }
+        return list;
+    }
     public static LinkedList add (LinkedList list, int data){
         Node newNode = new Node (data);
         if (list.head != null) {
-            Node temp = list.head;
-            newNode.next = temp;
+            newNode.next = list.head;
         }
         list.head = newNode;
+        return list;
+    }
+    public static LinkedList reverse (LinkedList list){
+        Node prev, curr, next;
+        curr = list.head;
+        prev = null;
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        list.head = prev;
         return list;
     }
     public static void printLinkedList (LinkedList list){
@@ -42,6 +90,14 @@ public class LinkedList {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
-
+        System.out.println("\n");
+    }
+    public static void printLinkedListRecursion (Node head){
+        if (head == null){
+            System.out.println("\n");
+            return;
+        }
+        System.out.print(head.data + " ");
+        printLinkedListRecursion(head.next);
     }
 }
